425f3f29e2b5c4e3f78ccbee4d788c439eeb7c6c
vuillaut <thomas.vuillaume@gmail.com>
2022-01-25 15:24:51 +0100

    add unit tests

---
+++ tests/test_generate.py
@@ -0,0 +1,6 @@
+from gitlabrng import generate_release_notes
+
+
+def test_generate_release_notes():
+    # TODO: replace with a dedicated test repo
+    notes = generate_release_notes(14117, url='https://gitlab.in2p3.fr')
---
+++ tests/test_scripts.py
@@ -0,0 +1,4 @@
+from subprocess import run
+
+run(['gitlab-release-notes', '14117', '--url', 'https://gitlab.in2p3.fr'])
+
